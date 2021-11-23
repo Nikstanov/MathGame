55f111fa50ce2b239e0c08e55a493163cd6b33d8
Nikstanov <n.stahnov@gmail.com>
2021-11-23 23:24:30 +0300

    hello

---
+++ src/test/java/com/epicteam/mathgame/GenerationTest.java
@@ -0,0 +1,46 @@
+package com.epicteam.mathgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerationTest {
+
+    @Test
+    void generator() {
+        Generation gen = new Generation(1,1);
+        assert(100 < gen.layoutQuestion2Y);
+    }
+
+    @Test
+    void cubicEquationsEasy() {
+    }
+
+    @Test
+    void qubeEquationsHard() {
+    }
+
+    @Test
+    void quadEquationsEasy() {
+    }
+
+    @Test
+    void hardEquationsEasy() {
+    }
+
+    @Test
+    void easyEquationsEasy() {
+    }
+
+    @Test
+    void hardEquationsEasy1() {
+    }
+
+    @Test
+    void znak() {
+    }
+
+    @Test
+    void fraction() {
+    }
+}
